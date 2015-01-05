f57b2669f7294921b951dd817f3bb01bc6003a30
Joerg Steinkamp <joerg.steinkamp@senckenberg.de>
2015-01-05 14:20:50 +0100

    added datasets for benchmarks and rainfall seasonality function

---
+++ R/rainfall_seasonality.R
@@ -0,0 +1,100 @@
+# A series of values (len) is transformed to vectors
+# with origin (0,0) and a cumulative angle of wgt.
+# As plot this would result in a windrose like picture.
+# These vectors are appended one after the other, leading
+# to a vector as result in direction of the months with
+# the highest precipitation. The sum of len divided by
+# the length of the vector sum is the seasonality.
+#
+# Usage is not limited to rainfall seasonality.
+# wgt can be defined as anything you like, which 
+# can be arranged circular and appended afterwards.
+#
+# This calculation is based on
+#
+# Charles G. Markhan (1970) Seasonality of precipitation
+# in rhe United States, Annals of the Association of 
+# American Geographers, 60:3, 593-597,
+# DOI: 10.1111/j.1467-8306.1970.tb00743.x
+
+rainfall_seasonality <- function(
+    len=c(10,  8,  6,  5,  4,  3,  1,  1,  2,  5,  7,  9),
+    wgt=c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31),
+    debug=FALSE) {
+  len.dim <- dim(len)
+  if (!is.vector(wgt)) {
+      warning("wgt is not a vector")
+      return
+  }
+  if (length(len.dim) <= 1) {
+    if (length(wgt) != length(len)) {
+      warning(paste("Input vector length differ(len: ", length(len), "; wgt: ", length(wgt), sep=""))
+      return
+    }
+  } else {
+    if (len.dim[length(len.dim)] != length(wgt)) {
+      warning(paste("Length of wgt and last dimension of len differ (len: ", len.dim[length(len.dim)], "; wgt: ", length(wgt), sep=""))
+      return
+    }
+  }
+
+  if (is.vector(len)) {
+    setClass("rf_seasonality",
+             representation(angle="numeric",
+                            vx="numeric", vy="numeric",
+                            seasonality="numeric"),
+             prototype=list(angle=numeric(),
+                 vx=numeric(), y=numeric(),
+                 seasonality=numeric()))
+  } else {
+    setClass("rf_seasonality",
+             representation(angle="numeric",
+                            vx="array", vy="array",
+                            seasonality="array"),
+             prototype=list(angle=numeric(),
+                 vx=array(NA, len.dim), y=array(NA, len.dim),
+                 seasonality=array(NA, len.dim)))
+  }
+  res <- new("rf_seasonality")
+
+  # calculate the wheighted angles based on wgt
+  cwgt <- cumsum(wgt) - wgt/2
+  cwgt <- append(cwgt, cwgt[length(cwgt)] + wgt[length(cwgt)]/2)
+  angle <- 2 * pi * cwgt / cwgt[length(cwgt)]
+  res@angle = angle
+
+  # calculate the x and y component of each vector
+  if (is.vector(len)) {
+    vx <- sin(angle[1:(length(angle)-1)]) * len
+    vy <- cos(angle[1:(length(angle)-1)]) * len
+    # plot(cumsum(vx),cumsum(vy), type="l")
+    # lines(c(0,cumsum(vx)[12]), c(0,cumsum(vy)[12]), col="red")
+  } else {
+    angle <- array(rep(angle[1:(length(angle)-1)], each=prod(len.dim[1:(length(len.dim)-1)])), c(len.dim))
+    vx <- sin(angle) * len
+    vy <- cos(angle) * len
+  }
+  res@vx = vx
+  res@vy = vy
+
+  # calculate the "seasonality" by summing up the x and y components
+  # of the vectors and divivion by the total precipitation
+  if (is.vector(len)) {
+    res@seasonality = (sqrt(cumsum(vx)[12]^2 + cumsum(vy)[12]^2)) / sum(len)
+  } else {
+    cs.vx <- apply(vx, 1:(length(len.dim)-1), cumsum)
+    cs.vy <- apply(vy, 1:(length(len.dim)-1), cumsum)
+    s.len <- apply(len, 1:(length(len.dim)-1), sum)
+
+    dim.str <- paste(len.dim[length(len.dim)])
+    for (i in 1:(length(len.dim)-1)) {
+      dim.str <- paste(dim.str, ",")
+    }
+    res@seasonality = eval(parse(text=paste("(sqrt(cs.vx[", dim.str, "]^2 + cs.vy[", dim.str, "]^2)) / s.len")))
+  }
+  if (debug) {
+    return(res)
+  } else {
+    return(res@seasonality)
+  }
+}
